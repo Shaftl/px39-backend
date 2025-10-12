195980b91bf8b812a316b7c1c566886b050c0e11
Shaftl <shaftlkhan990@gmail.com>
2025-10-12 16:55:12 +0430

    first commit

--- backend/routes/products.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const pc = require("../controllers/product.controller");
-
-// Public browsing
-router.get("/", pc.getAllProducts);
-router.get("/:id", pc.getProductById);
-
-module.exports = router;
